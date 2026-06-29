0eb0cedfa41b8799e8098a57ce816b1f25663895
adsmartup <adsmartup@users.noreply.github.com>
2026-06-29 21:18:10 +0300

    29.21.18

---
+++ includes/Schema/AggregateRating.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Senso\Schema\Schema;
+
+defined('ABSPATH') || exit;
+
+/**
+ * Reserved for v0.5.
+ *
+ * AggregateRating generation will be moved here.
+ */
+final class AggregateRating
+{
+}
