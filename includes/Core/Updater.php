26af2708c2a62ec9444a2c4f333fab8f5424f084
adsmartup <adsmartup@users.noreply.github.com>
2026-07-01 15:59:33 +0300

    Add updater wrapper

--- includes/Core/Updater.php
+++ includes/Core/Updater.php
@@ -1,1 +1,23 @@
 <?php
+
+declare(strict_types=1);
+
+namespace Senso\Schema\Core;
+
+use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+final class Updater
+{
+    public static function init(): void
+    {
+        PucFactory::buildUpdateChecker(
+            'https://w2x.org/updates/schema.json',
+            SENSO_SCHEMA_FILE,
+            'senso-schema'
+        );
+    }
+}
